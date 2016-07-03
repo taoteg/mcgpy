59f1a3a8990e89566ec0a9a826ec130eb50798d8
John Gentle <taoteg@gmail.com>
2016-07-02 21:08:47 -0500

    Fixed parameterization script.

--- parameterize-well-scalars.py
+++ parameterize-well-scalars.py
@@ -8,32 +8,53 @@ wells_src_path = os.path.abspath(wells_src_dir)
 wells_dest_dir = 'parameterized_well_scalars'
 wells_dest_path = os.path.abspath(wells_dest_dir)
 
+print ' '
+print '>>> START GAME <<<'
+print ' '
+# print 'wells_src_dir: ', wells_src_dir
+print 'wells_src_path: ', wells_src_path
+# print 'wells_dest_dir: ', wells_dest_dir
+print 'wells_dest_path: ', wells_dest_path
 
-for well in wells_src_path:
+well_src_files = os.listdir(wells_src_path)
+print ('well_src_files %s' % well_src_files)
+# print ' '
+
+for well_src in well_src_files:
+
     groupCount = 0
     wellCount = 0
 
-    while (groupCount < 81):
+    while (groupCount < 3): #81
 
         # Build directory name for case.
-        current_group_dir = wells_dest_path + '/well_scalars_' + str(groupCount)
-
-        print ('The current group directory is %s' % current_group_dir)
+        current_group_dir = 'well_scalars_' + str(groupCount)
+        current_group_dir_path = wells_dest_path + '/' + current_group_dir
 
         # Create directory if it does not exist.
-        if not os.path.exists(current_group_dir):
-            os.makedirs(current_group_dir)
+        if not os.path.exists(current_group_dir_path):
+            os.makedirs(current_group_dir_path)
 
-        if (wellCount < 119):
-            print ('The current well file is %s' % well)
-            shutil.copy(well, current_group_dir)
-            print ('The wellCount is %s' % wellCount)
+        if (wellCount < 3): #119
+            print '----------------------------------------------------------------------------'
+            current_well_file = wells_src_path + '/' + well_src
+            print ('The current_well_file is: %s' % current_well_file)
+            # print ('The current well_src is: %s' % well_src)
+            print ('The current_group_dir_path is: %s' % current_group_dir_path)
+
+            if (os.path.isfile(current_well_file)):
+                shutil.copy(current_well_file, current_group_dir_path)
+            print ('The wellCount is: %s' % wellCount)
             wellCount = wellCount + 1
+            # quit()
         else:
             groupCount = groupCount + 1
             wellCount = 0
-            print ('The groupCount is now %s' % groupCount)
-            print ('The wellCount is now %s' % wellCount)
+            print '++++++++++++++++++++++++++++++++++++++++++'
+            print ('The groupCount is: %s' % groupCount)
+            print ('The wellCount is: %s' % wellCount)
+            # quit()
 
-    else:
-        quit()
+else:
+    print '!!! GAME OVER !!!'
+    quit()
