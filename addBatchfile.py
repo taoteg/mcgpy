a5e844ce414f0a0853de91fc4f760ba898559df8
John Gentle <taoteg@gmail.com>
2016-07-05 12:20:40 -0500

    tweaking addbatch ascript.

--- addBatchfile.py
+++ addBatchfile.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+
+"""
+MODFLOW 96 Add Batchfile (Python)
+MAB.PY
+v.1.0.0
+By John Gentle
+Updated 2016.07.05
+
+Python script to add a vanilla bathcfile to the case scenario input files for use in MODFLOW 96.
+See README for usage instructions.
+"""
+
+# Run script from parent dir of case folders.
+
 ####################################################################
 # Imports.
 ####################################################################
@@ -7,11 +22,9 @@ import os
 
 # current_dir = os.path.abspath('')
 current_dir = os.getcwd()
-
-# Run script from parent dir of case folders.
-modelsourcedir = '.'
+modelsourcedir = current_dir
 model_src_dir = modelsourcedir
-print model_src_dir
+# print model_src_dir
 model_src_path = os.path.abspath(model_src_dir)
 print model_src_path
 
